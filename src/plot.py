2a2f49af048f5ea2c16fbf2809beda4cd0a629e6
amritbhanu <amritbhanu@gmail.com>
2018-09-24 13:22:46 +0530

    results for 20 repeats

--- src/plot.py
+++ src/plot.py
@@ -9,25 +9,20 @@ from collections import OrderedDict
 from operator import itemgetter
 from sklearn.metrics import auc
 
-e_value=[0.025,0.05,0.1,0.2]
-files=["ant","camel"]
+e_value=[0.2,0.1, 0.05,0.025]
+files=["ivy","camel","jedit","log4j","lucene","poi","synapse","velocity","xalan","xerces"]
 #files=["ivy","log4j","synapse","velocity", "ant","arc","camel","poi","prop","velocity","jedit"
 #       ,"log4j","redaktor","tomcat","xalan","xerces"]
 
 ROOT=os.getcwd()
 
 def dump_files(f=''):
-    final={}
-    for _, _, files in os.walk(ROOT + "/../dump/d2h/"):
-        for file in files:
-            if f in file:
-                with open("../dump/d2h/" + file, 'rb') as handle:
-                    dic = pickle.load(handle)
-                    dic1=OrderedDict(sorted(dic.values()[0].items(), key=itemgetter(0)))
-                    dic[dic.keys()[0]]=dic1.values()
-                    final.update(dic)
-    fn=OrderedDict(sorted(final.items(), key=itemgetter(0)))
-    return fn
+    # for _, _, files in os.walk(ROOT + "/../dump/defect/"):
+    #     for file in files:
+    #         if f in file:
+    with open("../dump/defect/popt20_" + f+".pickle", 'rb') as handle:
+        final = pickle.load(handle)
+    return final
 
 
 def draw(dic,f):
@@ -39,21 +34,29 @@ def draw(dic,f):
     colors = ['red', 'green', 'blue', 'orange']
     markers=["o","*","v","D"]
     fig = plt.figure(figsize=(80, 60))
-    for x,i in enumerate(dic.keys()):
-        plt.plot(dic[i],color=colors[x],label=str(i)+" epsi")
+    for x,i in enumerate(e_value):
+        li=dic[i]
+        li=[y+(0.01*(x+1)) for y in li]
+        plt.plot(li,color=colors[x],label=str(i)+" epsi")
 
-    plt.ylabel("Min D2h Score")
+    plt.ylabel("Max popt Score")
+    plt.ylim(0,1)
     plt.xlabel("No. of iterations")
     plt.legend(bbox_to_anchor=(0.7, 0.5), loc=1, ncol=1, borderaxespad=0.)
-    plt.savefig("../results/d2h/"+f+ ".png")
+    plt.savefig("../results/popt/"+f+ ".png")
     plt.close(fig)
 
+def draw_boxplot(dic,f):
+    pass
 
 if __name__ == '__main__':
     temp_fi={}
     for i in files:
+        print(i)
         dic=dump_files(i)
-        print(round(auc(list(range(len(dic[0.025]))),dic[0.025]),3))
-        #print(dic[0.025])
+        ## draw the graph
+        #draw(dic['temp'],i)
+        del dic["temp"]
+        del dic["time"]
+        draw_boxplot(dic,i)
         break
-        #draw(dic,i)
